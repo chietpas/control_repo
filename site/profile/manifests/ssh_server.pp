59da4ea38ba363ddb2ad1dbf7e510c4c541817b7
chietpas <72933005+chietpas@users.noreply.github.com>
2020-10-16 13:55:08 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC2RGbmJuuWLiMoGCsziUgnLi4MFwUnl2Xt1S3pphUZHbqS/qb+3epTgx6xuDj1lI9oC1xDAaLzsrM/Ytu0cUojd3442KM3J/98qmrpUh8NzXabgAOimzl5SPhM0SNVZdLdbj1mr6JKQ8ukaYgWwRpBesJmYv5zpvZkycdVWk6P7DBiCkEhIJVffoju/EQNYzzrCkgmYawzbwd2+IKxeSLBc2WAHt1eIIeZnsINwVmqCOqQGhUWPbsa5oN0INT0IwOcYPrNBoDG93je3MxbZP9xqXvzYXcrrER/P89BQPtuGlMlcc3TVLZZ7lSULgJuJziWxqHArSR+USCIa1k9N/jJ',
 	}  
 }
